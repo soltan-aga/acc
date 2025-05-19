1e715e80a64252f6fe2dbc1f9ff1699f8e29700c
soltan-aga <soltan-aga@outlook.com>
2025-05-20 01:26:35 +0300

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -1,11 +1,12 @@
-import os
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 
-User = get_user_model()
-
-username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+try:
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='giltmole'
+    )
+    print('Superuser created successfully')
+except IntegrityError:
+    print('Superuser already exists')
